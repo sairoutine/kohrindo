5a7d77388e43b003e1fc1acd049170168e254cd5
sairoutine <sairoutine@gmail.com>
2015-11-06 16:06:44 +0900

    Add middleware of common execution

--- app.js
+++ app.js
@@ -132,6 +132,55 @@ app.use(passport.initialize()); // passportの初期化処理
 app.use(passport.session()); // passportのsessionを設定(中身はしらぬ)
 // -- 追加ココまで --
 
+// テンプレに渡す共通変数はここで定義する。
+app.use(function (req, res, next) {
+	// SUPER::render
+	var _render = res.render;
+
+	/* ユーザーがログイン済かどうか */
+	var isAuthenticated = req.isAuthenticated();
+
+	/* res.render をオーバーライド */
+	res.render = function(view, options, fn) {
+		/* 認証しているか否か */
+		options.isAuthenticated = isAuthenticated;
+
+		/* ユーザーデータを追加する */
+		options.mydata = {};
+
+		/* ログインしてなければユーザーデータを追加しない */
+		if(!req.isAuthenticated()) {
+        	_render.call(this, view, options, fn);
+			return;
+		}
+
+		/* ユーザーの投稿数 */
+		knex.select(
+			knex.raw('count(*) as impression_num')
+		)
+		.from('impression')
+		.where('user_id', req.user)
+		.then(function(rows) {
+			options.mydata.impression_num = rows[0].impression_num;
+
+			/* ユーザー名とユーザー画像 */
+			return knex.select('displayname', 'thumbnail')
+			.from('user')
+			.where('id', req.user);
+		})
+		.then(function(rows) {
+			options.mydata.profile = rows[0];
+
+			// call SUPER::render
+			_render.call(res, view, options, fn);
+		})
+		.catch(function(err) {
+			throw err;
+		});
+
+    };
+    next();
+});
 
 // indexページ
 var routes      = require('./routes/index');
--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,9 @@ router.get('/', function(req, res, next) {
 
  	/* viewに渡すパラメータ */
 	var data = {
-		/* ログイン後のユーザー情報 */
-		mydata: {},
 		/* 最近投稿された感想 */
 		impression: []
 	};
-
-	/* 認証しているか否か */
-	data.isAuthenticated = req.isAuthenticated();
-
 
 	/* 同人誌の感想一覧を取得 */
 	knex.select('id', 'doujinshi_id', 'body', 'create_time')
@@ -53,36 +47,7 @@ router.get('/', function(req, res, next) {
 			data.impression[i].doujinshi = doujinshi_id_row_hash[data.impression[i].doujinshi_id];
 		}
 
-		/* 認証してなければそのまま描画 */
-		if(!req.isAuthenticated()) {
-			res.render('index', data);
-			return;
-		}
-
-		/* 以下、ユーザーデータの取得 */
-		knex.select(
-			knex.raw('count(*) as impression_num')
-		)
-		.from('impression')
-		.where('user_id', req.user)
-		.then(function(rows) {
-			data.mydata.impression_num = rows[0].impression_num;
-
-			return knex.select('displayname', 'thumbnail')
-			.from('user')
-			.where('id', req.user);
-		})
-		.then(function(rows) {
-			data.mydata.profile = rows[0];
-
-			res.render('index', data);
-		})
-		.catch(function(err) {
-			next(err);
-		});
-	})
-	.catch(function(err) {
-		next(err);
+		res.render('index', data);
 	});
 });
 
